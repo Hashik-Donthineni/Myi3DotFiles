a6d9e6dbfe2734d62bbf5af151de50e35e694098
Hashik Donthineni <hashikdonthineni@gmail.com>
2020-03-22 09:58:41 +0530

    init commit

---
+++ detectdiplays.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+command=$(xrandr --query | grep " connected"|cut -d " " -f1)
+count=$(xrandr --query | grep " connected"|cut -d " " -f1| wc -w)
+
+if [ "$count" -eq 2 ];
+then
+xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output eDP-1 --off --output DP-2 --primary --mode 1680x1050 --pos 0x0 --rotate normal
+else
+xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output eDP-1 --mode 1366x768 --pos 0x0 --rotate normal --output DP-2 --off
+fi
