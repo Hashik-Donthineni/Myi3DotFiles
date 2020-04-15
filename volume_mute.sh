7c6c5d435643f880ac6575370b9bb21d46113ed3
HashikDonthineni <hashikdonthineni@gmail.com>
2020-04-15 17:37:06 +0530

    Added support for bluetooth speaker volume

---
+++ volume_mute.sh
@@ -0,0 +1,7 @@
+total_dev=$(pactl list short sinks | wc -l)
+echo "Volume Increase" >> ~/.config/i3/log
+
+id=$(pactl list short sinks |tail -1| cut -c1-2)
+
+echo $id >> ~/.config/i3/log
+pactl set-sink-mute $id toggle
